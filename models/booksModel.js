5a556da665a6ecce4aa66fdcd637718873acaf29
AN VÕ THIỆN <voan0935995700@gmail.com>
2020-12-01 14:13:26 +0700

    add delete and add book functions in model

--- models/booksModel.js
+++ models/booksModel.js
@@ -13,3 +13,40 @@ exports.get = async id => {
   const book = await bookCollection.findOne({ _id: ObjectID(id) });
   return book;
 };
+
+exports.addBook = async bookObj => 
+{
+    const bookCollection = await db().collection("Books");
+    let success = true; 
+    let bookAdded =  await bookCollection.insertOne(bookObj); 
+
+    
+
+    if(bookAdded === null || bookAdded === undefined)
+    {
+      success = false; 
+    }
+
+
+     return success; 
+}
+
+exports.deleteBook = async id => 
+{
+  const bookCollection = await db().collection("Books");
+  let success = false; 
+  let existsBook = await bookCollection.findOne({_id: ObjectID(id)});
+  
+  //console.log(existsBook);
+
+  if(existsBook === null || existsBook === undefined)
+  {
+      console.log(`Can't find book with ID ${id}`);
+      success = false; 
+  }
+  else{
+     await bookCollection.deleteOne({_id: ObjectID(id)});
+     success = true; 
+  }
+  return success; 
+}
