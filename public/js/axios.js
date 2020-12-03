be9c57afad85f17cc71614b6cebc46fe0a2094b3
Hoa-Vo <dev.h1620@gmail.com>
2020-12-03 12:12:59 +0700

    fix address to deploy on heroku

--- public/js/axios.js
+++ public/js/axios.js
@@ -45,7 +45,7 @@ const previewImage = function (event) {
   reader.readAsDataURL(event.target.files[0]);
 };
 const form = document.getElementById("submitForm");
-form.addEventListener('submit', e => {
+form.addEventListener("submit", e => {
   e.preventDefault();
   const files = document.getElementsByName("book-image")[0].files[0];
   const title = document.getElementsByName("titleInput")[0].value;
@@ -53,23 +53,24 @@ form.addEventListener('submit', e => {
   const author = document.getElementsByName("authorInput")[0].value;
   const publisher = document.getElementsByName("publisherInput")[0].value;
   const formData = new FormData();
-  formData.append('bookImage', files);
+  formData.append("bookImage", files);
   async function run(url) {
-    const response=await axios.post(url,formData);
+    const response = await axios.post(url, formData);
     return response;
   }
-  formData.append('title',title);
-  formData.append("basePrice",basePrice);
-  formData.append("author",author);
-  formData.append("publisher",publisher);
-  run("http://localhost:5500/bookslist/createNew").then(res=>{
-    return res.data.imageName;
-  })
-      .then(imageName=>{
-        formData.append("imageName",imageName);
-        run("http://localhost:3000/api/admin-send-image").then(res=>{
-          console.log(res);
-          alert("Them thanh cong!!");
-        });
-      })
+  formData.append("title", title);
+  formData.append("basePrice", basePrice);
+  formData.append("author", author);
+  formData.append("publisher", publisher);
+  run("https://admin-bookland.herokuapp.com/bookslist/createNew")
+    .then(res => {
+      return res.data.imageName;
+    })
+    .then(imageName => {
+      formData.append("imageName", imageName);
+      run("https://bookland-hcmus.herokuapp.com/api/admin-send-image").then(res => {
+        console.log(res);
+        alert("Them thanh cong!!");
+      });
+    });
 });
