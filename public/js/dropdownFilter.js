f4cf8a0e8c53cb464f686e0bd362f70be3fc3ead
AN VÕ THIỆN <voan0935995700@gmail.com>
2020-12-07 20:03:49 +0700

    fix small UI (dropdown) error

--- public/js/dropdownFilter.js
+++ public/js/dropdownFilter.js
@@ -2,13 +2,17 @@ function toogleHideAndShowDropDown()
 {
     var childDiv = document.getElementById("filter-dropdown");
     var a = childDiv.getElementsByTagName("a");
-    if(childDiv.style.display == "none")
+    //console.log(childDiv.style.display);
+    if(childDiv.style.display === "block")
     {
-        childDiv.style.display = ""; 
+        childDiv.style.display = "none"; 
+        console.log("Switch to display none");
     }
     else
     {
-        childDiv.style.display="none";
+        console.log("Switch to block");
+
+        childDiv.style.display="block";
     }
     
 }
