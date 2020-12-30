6972fdc9b45a8bfcfa2e1963cc4d11c47a93e835
Hoa-Vo <dev.h1620@gmail.com>
2020-12-30 19:12:13 +0700

    fix bug category when search

--- controllers/searchSpecifiedBookController.js
+++ controllers/searchSpecifiedBookController.js
@@ -1,15 +1,15 @@
-const booksListModel = require("../models/booksModel.js");
+const booksModel = require("../models/booksModel.js");
 
+exports.get = async (req, res, next) => {
+  const bookName = req.query.bookName;
+  const books = await booksModel.searchBook(bookName);
+  const categoriesListToShowInMenu = await booksModel.getAllCategory();
+  // Pass data to view to display list of books
 
-exports.get=async (req,res,next)=>{
-    const bookName=req.query.bookName;
-    console.log(bookName);
-    const books = await booksListModel.searchBook(bookName);
-    console.log(books);
-    if (books==null) console.log("Không tìm thấy");
-  else {
-    console.log("Tìm thấy");
-    console.log();
-  }
-  res.render("booksPage/bookslist", { books, bookName });
+  res.render("booksPage/bookslist", {
+    books,
+    bookName,
+    categories: categoriesListToShowInMenu,
+    currentCategory: "Tất cả",
+  });
 };
