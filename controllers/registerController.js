74b0d3ad66a42ce0fd278f905b75979f3a0f0ef8
truongtrieu123 <uerittruong@gmail.com>
2021-01-14 17:02:21 +0700

    only super -admin can register new account

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -2,10 +2,25 @@ const accountModel = require("../models/accountModel");
 const accountServices = require("../services/accountServices");
 
 exports.renderRegisterPage = (req, res, next) => {
-  res.render("./login-register/register");
+  let userToShow=null;
+  if(req.user)
+  {
+    const id=req.user._id;
+    const userToShow=accountModel.getUserById(id);
+    res.render("./login-register/register", {userToShow:userToShow});
+  }
+  else 
+  {
+    res.redirect("./login");
+  }
 };
 
 exports.registerNewUser = async (req, res, next) => {
-  await accountServices.registerNewuser(req.body.username, req.body.password, req.body.email);
+  if(req.user)
+  {
+    const adminID=req.user._id;
+    const registered=await accountServices.registerNewuser(req.user._id,req.body.username, req.body.password, req.body.email);
+    console.log(registered.success);
+  }
   res.redirect("/login");
 };
