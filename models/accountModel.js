d9b77cb591120eb12e6157c89311ab599a566466
truongtrieu123 <uerittruong@gmail.com>
2021-01-14 17:01:05 +0700

    update accountModel, use adminAccount

--- models/accountModel.js
+++ models/accountModel.js
@@ -7,34 +7,37 @@ const { constants } = require("crypto");
 
 // get user by ID
 exports.getUserById = async id => {
-  const userCollection = await db().collection("registeredUser");
-  const user = await userCollection.findOne({ _id: ObjectID(id) });
+  const adminAccountCollection = await db().collection("adminAccount");
+  const user = await adminAccountCollection.findOne({ _id: ObjectID(id) });
   return user;
 };
 
 // get user by username
 exports.getUserByUsername = async name => {
-  const userCollection = await db().collection("registeredUser");
-  const user = await userCollection.findOne({ name: name });
+  const adminAccountCollection = await db().collection("adminAccount");
+  const user = await adminAccountCollection.findOne({ name: name });
   return user;
 };
 // list all user
 exports.listAll = async () => {
-  const userCollection = await db().collection("registeredUser");
-  const users = await userCollection.find({}).toArray();
+  const adminAccountCollection = await db().collection("adminAccount");
+  const users = await adminAccountCollection.find({}).toArray();
   return users;
 };
 
 // try add new user
 exports.addNewUser = async function (newUsername, plainNewPassword, newEmail) {
-  const userCollection = await db().collection("registeredUser");
+  const adminAccountCollection = await db().collection("adminAccount");
   const userpasswordCollecton = await db().collection("User-hashPassword");
-  const newInserted = await userCollection.insertOne({
+  const newInserted = await adminAccountCollection.insertOne({
     name: newUsername,
     age: 10,
     email: newEmail,
+    role:"admin",
     avatar_image: "notfound.jpg",
     isVerified: false,
+    isLocked:false,
+    super_admin:false,
   });
   console.log("New inserted user object");
 
@@ -50,13 +53,13 @@ exports.addNewUser = async function (newUsername, plainNewPassword, newEmail) {
 // toogle verify (to true)
 
 exports.changeVerifyStatus = async (id, newVerifyStatus) => {
-  const userCollection = await db().collection("registeredUser");
-  await userCollection.updateOne({ _id: ObjectID(id) }, { $set: { isVerified: newVerifyStatus } });
+  const adminAccountCollection = await db().collection("adminAccount");
+  await adminAccountCollection.updateOne({ _id: ObjectID(id) }, { $set: { isVerified: newVerifyStatus } });
 };
 
 exports.isExistsUsername = async inputUsername => {
-  const userCollection = await db().collection("registeredUser");
-  let userDocument = await userCollection.findOne({ name: inputUsername });
+  const adminAccountCollection = await db().collection("adminAccount");
+  let userDocument = await adminAccountCollection.findOne({ name: inputUsername });
   if (userDocument) {
     return true;
   } else {
@@ -65,9 +68,9 @@ exports.isExistsUsername = async inputUsername => {
 };
 
 exports.isExistsEmail = async inputEmail => {
-  const userCollection = await db().collection("registeredUser");
-  const userDocument = await userCollection.findOne({ email: inputEmail });
-  if(userDocument)
+  const adminAccountCollection = await db().collection("adminAccount");
+  const adminAccountDocument = await adminAccountCollection.findOne({ email: inputEmail });
+  if(adminAccountDocument)
   {
     return true; 
   }
@@ -75,40 +78,4 @@ exports.isExistsEmail = async inputEmail => {
     return false;
   }
   
-}
-
-exports.lockAccount=async id =>
-{
-  const accountCollection =await db().collection("registeredUser");
-  let success = false;
-  let existsAccount = await accountCollection.findOne({ _id: ObjectID(id) });
-  
-  if (existsAccount === null || existsAccount === undefined) {
-    console.log(`Can't find users with ID ${id}`);
-    success = false;
-  } else {
-    if (existsAccount.isLocked === false) {
-      await accountCollection.updateOne({ _id: ObjectID(id) }, { $set: { isLocked: true } });
-    } 
-    success = true;
-  }
-  return { success };
-}
-
-exports.unlockAccount=async id =>
-{
-  const accountCollection =await db().collection("registeredUser");
-  let success = false;
-  let existsAccount = await accountCollection.findOne({ _id: ObjectID(id) });
-  
-  if (existsAccount === null || existsAccount === undefined) {
-    console.log(`Can't find users with ID ${id}`);
-    success = false;
-  } else {
-    if (existsAccount.isLocked === true) {
-      await accountCollection.updateOne({ _id: ObjectID(id) }, { $set: { isLocked: false } });
-    } 
-    success = true;
-  }
-  return { success };
 }
